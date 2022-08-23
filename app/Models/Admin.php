0310f3c006c9fc650731e3dd4302ce21fdfb295c
Your Name <you@example.com>
2022-08-23 09:06:01 +0700

    LaiHung update Admin and product ver 1.0

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,20 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
 
-
-class Admin extends Model
-{
-    protected $table =  'admins';
-    protected $fillable =[
-            'name',
-            'password',
-            'image',
-            'status',
-    ];
-
-=======
 
 use App\Models\Category;
 
@@ -27,6 +14,6 @@ class Admin extends Model
     public $primaryKey = 'id';
     public $fillable = ['name'];
     public $timestamps = false;
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
+
 
 }
--- app/Models/Products.php
+++ app/Models/Products.php
@@ -9,23 +9,12 @@ use App\Models\Product;
 
 class Products extends Model
 {
-    // use HasFactory;
-<<<<<<< HEAD
-
-    protected $fillable = ['title','price','description','image','id','updated_at', 'created_at'];
-    public function category(){
-        return $this->belongsTo(Category::class);
-    }
-    protected $table = 'products';
-
-    protected $primaryKey = 'id';
-=======
+   
     protected $fillable = ['title','price','description','image','category_id','updated_at', 'created_at'];
     // public function category(){
     //     return $this->belongsTo(Category::class);
     // }
     protected $table = 'product';
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
 
     
 
