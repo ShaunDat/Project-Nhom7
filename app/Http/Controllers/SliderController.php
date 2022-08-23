bd5e34866b3be2939193893887277272bff4dd65
Your Name <you@example.com>
2022-08-23 09:04:44 +0700

    LaiHung update slider ver 1.0

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Slider;
-
+class SliderController extends Controller
+{
     /**
      * Show the form for creating a new resource.
      *
