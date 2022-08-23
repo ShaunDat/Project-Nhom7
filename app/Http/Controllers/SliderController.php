59138249fdb2090ed305d09cb55f4c36b476d878
Your Name <you@example.com>
2022-08-23 09:03:16 +0700

    LaiHung update product ver 1.2

--- app/Http/Controllers/SliderController.php
+++ app/Http/Controllers/SliderController.php
@@ -2,58 +2,28 @@
 
 namespace App\Http\Controllers;
 
-<<<<<<< HEAD
 
-use App\Models\Slider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
-
-class SliderController extends Controller
-{
-    public function index()
-    {
-        $slider = Slider::all();
-        return view('manager.slider.index',compact('slider'));
-    }
-
-    
-    
-=======
 use Illuminate\Http\Request;
 use App\Models\Slider;
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
 
-
-<<<<<<< HEAD
-
-
-    
-   
-    
-=======
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
+
     public function create()
     {
         //
         return view('manager.slider.create');
     }
 
-<<<<<<< HEAD
-    
-
-=======
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
     public function store(Request $request)
     {   
         $slider = new Slider();
@@ -75,49 +45,6 @@ use App\Models\Slider;
         return redirect()->back()->with('status','added slider Successfully');
     }
 
-
-<<<<<<< HEAD
-   
-   
-
-    
-
-
-   
-    public function edit($id)
-    {
-        $slider = Slider::find($id);
- 
-        return view('manager.slider.edit',compact('slider'));
-    }
-
-   
-    public function update(Request $request, $id)
-    {   
-        $slider = Slider::find($id);
-        $slider->title = $request->input('title');
-        $slider->description= $request->input('description');
-        $slider->status= $request->input('description');
-
-        if($request->hasfile('image'))
-        {
-            $destination = 'uploads/banner/'.$slider->image;
-            if(File::exists($destination)){
-                   File::delete($destination);
-            }
-            $file = $request->file('image');
-            $extention = $file->getClientOriginalExtension();
-            $filename = time().'.'.$extention;
-            $file->move('uploads/banner/',$filename);
-            $slider->image = $filename ;
-
-        }
-        $slider->status = $request->input('status') == true ? '1':'0';
-        $slider->save();
-        return redirect()->back()->with('status','slider update Successfully');
-    }
-
-=======
     /**
      * Display the specified resource.
      *
@@ -129,7 +56,6 @@ use App\Models\Slider;
         //
     }
     
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
     
     public function destroy(Slider $slider)
     {
@@ -145,11 +71,6 @@ use App\Models\Slider;
         
         return redirect()->back()->with('status','slider Delete Successfully');
         }
-<<<<<<< HEAD
-        return redirect()->back()->with('status','error ');
-    }
-
-=======
     }
 
     /**
@@ -186,7 +107,6 @@ use App\Models\Slider;
     // {
     //     //
     // }
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
 }
     
 
--- resources/views/Products/Create.blade.php
+++ resources/views/Products/Create.blade.php
@@ -6,37 +6,10 @@
     <div class="card">
       <div class="card-header">
           <h2>Add New Product</h2>
-          {{-- <a class="btn btn-primary" href="{{ route('Products.Product') }}"> Back</a> --}}
       </div>
 
-<<<<<<< HEAD
- 
-
-{{-- @if ($errors->any())
-
-@if ($errors->any())
-
-  <div class="alert alert-danger">
-      <strong>Whoops!</strong> There were some problems with your input.<br><br>
-      <ul>
-          @foreach ($errors->all() as $error)
-              <li>{{ $error }}</li>
-          @endforeach
-      </ul>
-  </div>
-
-@endif --}}
- 
-<form action="{{ route('/Products/store') }}" method="POST" enctype="multipart/form-data">
-
-=======
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
 @endif
 <form action="{{ route('/product/store') }}" method="POST" enctype="multipart/form-data">
-<<<<<<< HEAD
-
-=======
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
   @csrf
 
    <div class="row">
@@ -68,15 +41,7 @@
               <strong>Category:</strong>
               <select name="category" class="form-control">
                   @foreach($category as $category)
-<<<<<<< HEAD
-
-                  <option value="{{$category->id}}">{{$category->name}}</option>
-
                   <option value="{{$category->category_id}}">{{$category->category_name}}</option>
-
-=======
-                  <option value="{{$category->category_id}}">{{$category->category_name}}</option>
->>>>>>> fc66f2e98782b780ee93495ae710db9d7c196658
                   @endforeach
                 </select>
           </div>
