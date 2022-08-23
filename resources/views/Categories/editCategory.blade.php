47a1dd9e92acb95e8c989da88f033fad770c4f80
Your Name <you@example.com>
2022-08-23 09:16:49 +0700

    LaiHung update category ver 1.2

--- resources/views/Categories/editCategory.blade.php
+++ resources/views/Categories/editCategory.blade.php
@@ -3,6 +3,7 @@
 @section('content')
 <form role ="form" action="" method="post">
     @csrf       
+    {{-- edit category --}}
         <label>Name</label>
         <input class="form-control" name="category_name" value ="{{$cate->category_name}}">       
     <button type="submit" class="btn btn-success">Submit Button</button>
